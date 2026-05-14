d5a505df1c3693528c831d429773195741c5a667
JCabrera07 <jeandavidphone07@gmail.com>
2026-05-14 06:13:17 -0500

    Configure CORS and environments

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   app.enableCors({
   origin: [
     'http://localhost:4200',
-    'https://kiddyquiz.onrender.com'
+    'https://grand-begonia-774f68.netlify.app'
   ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });
